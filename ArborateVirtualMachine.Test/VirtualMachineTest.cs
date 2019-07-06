848df1576b91e4cdcf75059d4068a8c4591379e9
Phil Hutchinson <phil.hutchinson@gmail.com>
2019-07-06 11:59:54 -0700

    Initial project
    Closes https://github.com/phil-hutchinson/arborate-cs-vm/issues/1

---
+++ ArborateVirtualMachine.Test/VirtualMachineTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ArborateVirtualMachine.Test
+{
+    public class VirtualMachineTest
+    {
+        [Fact]
+        public void Placeholder()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ ArborateVirtualMachine/VirtualMachine.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ArborateVirtualMachine
+{
+    public class VirtualMachine
+    {
+    }
+}
